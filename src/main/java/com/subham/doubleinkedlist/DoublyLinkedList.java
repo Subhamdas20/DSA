c14658aea1c4ea907b0ae78259a732b3120859de
Subhamdas20 <Subham21.sd@gmail.com>
2022-07-24 22:54:02 +0530

    [subham] : Added circular dll

--- src/main/java/com/subham/doubleinkedlist/DoublyLinkedList.java
+++ src/main/java/com/subham/doubleinkedlist/DoublyLinkedList.java
@@ -47,15 +47,114 @@ public class DoublyLinkedList {
         size++;
 
     }
+    public void traverseDLL(){
+        if(head!=null){
+            DoublyNode tempNode = head;
+            for(int i=0;i<size;i++){
+                System.out.print(tempNode.value);
+                if(i!=size-1){
+                    System.out.print("->");
+                }
+                tempNode=tempNode.next;
+            }
+            System.out.println();
+        }
+        else System.out.println("DLL does not exit");
+    }
+    public void reverseTraverse(){
+        if(head!=null){
+            DoublyNode tempNode = tail;
+            for (int i = 0; i<size;i++){
+                System.out.print(tempNode.value);
+                if(i!=size-1){
+                    System.out.print("->");
+                }
+                tempNode=tempNode.prev;
+            }
+            System.out.println();
+        }
+        else System.out.println("DLL not present");
+    }
+    public boolean searchDll(int value){
+        DoublyNode tempNode = head;
+        if(head!=null){
+            for(int i=0;i<size;i++){
+                if(tempNode.value==value){
+                    System.out.println("Node present");
+                    return true;
+                }
+                else tempNode=tempNode.next;
+            }
+        }
 
+            System.out.println("Node not found");
+            return false;
+
+    }
+    public void deleteDLL(int location){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        else if(location==0){
+            if(size==1) {
+                head = null;
+                tail = null;
+                size--;
+            }
+            else{
+                head=head.next;
+                head.prev=null;
+                size--;
+            }
+        }
+        else if(location>=size){
+            DoublyNode tempNode=tail.prev;
+            if(size==1) {
+                head = null;
+                tail = null;
+                size--;
+            }
+            else {
+                tempNode.next=null;
+                tempNode=tail;
+                size--;
+            }
+        }else{
+            DoublyNode tempNode = head;
+            for(int i=0;i<location-1;i++){
+                tempNode=tempNode.next;
+            }
+            tempNode.next=tempNode.next.next;
+            tempNode.next.prev=tempNode;
+            size--;
+        }
+    }
+
+    public void deleteEntireDLL(){
+        DoublyNode tempNode=head;
+        for(int i = 0;i<size;i++){
+            tempNode.prev=null;
+            tempNode=tempNode.next;
+        }
+        head=null;
+        tail=null;
+        System.out.println("Dll has been deleted");
+    }
     public static void main(String[] args) {
         DoublyLinkedList list = new DoublyLinkedList();
         list.createDLL(5);
-
         list.insertDLL(1, 1);
         list.insertDLL(5, 2);
+        list.insertDLL(8, 3);
         list.insertDLL(4, 5);
         System.out.println(list.head.next.next.next.value);
         System.out.println(list.tail.value);
+        list.traverseDLL();
+//        list.reverseTraverse();
+        list.searchDll(10);
+
+        list.deleteDLL(3);
+        list.traverseDLL();
+
     }
 }
